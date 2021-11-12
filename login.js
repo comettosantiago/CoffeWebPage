8fc0640125843dc03d06a6d64f8614dccecb5de6
comettosantiago <90621053+comettosantiago@users.noreply.github.com>
2021-11-11 22:57:56 -0300

    v1.0.3
    Registro mejorado con avisos

--- login.js
+++ login.js
@@ -21,8 +21,8 @@ function cambiarLogin() {
         login.setAttribute('style', 'visibility: visible; opacity:1');
         document.getElementById('changebutton').setAttribute('value', 'Registrarse')
     }
-}
 
+}
 
 //--------------------Validar registro--------------------
 function validar() {
@@ -30,6 +30,7 @@ function validar() {
     document.getElementById("reguser2").classList.remove('error');
     document.getElementById("regpassword").classList.remove('error');
     document.getElementById("regpassword2").classList.remove('error');
+    document.getElementById("contenedorerrores").classList.remove('mostrarerrores');
 
     var email = document.getElementById("reguser").value;
     var email2 = document.getElementById("reguser2").value;
@@ -38,24 +39,50 @@ function validar() {
 
     var expresionMail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+    let errores = [];
+
+    function displayerr() {
+        if (errores.length > 0) {
+            var cartel = "<p>";
+            for (var i = 0; i < errores.length; i++) {
+                cartel += errores[i] + "<br><br>";
+            }
+            cartel += "</p>"+ "<div id='exclm'><i class='fas fa-exclamation-triangle'></i></div>";
+
+            document.getElementById("contenedorerrores").classList.add('mostrarerrores');
+
+            document.getElementById("contenedorerrores").innerHTML = cartel;
+            return false;
+        }
+    }
+
     if (!expresionMail.test(email) || email != email2 || email.length === 0) {
         if (password != password2 || password.length < 6) {
             document.getElementById("regpassword").classList.add('error');
             document.getElementById("regpassword2").classList.add('error');
             document.getElementById("reguser").classList.add('error');
             document.getElementById("reguser2").classList.add('error');
+            errores.push('Las contraseñas deben coincidir y tener mas de 8 caracteres');
+            errores.push('Debe introducir un correo electronico valido y repetirlo correctamente');
+            displayerr();
             return false;
         } else {
             document.getElementById("reguser").classList.add('error');
             document.getElementById("reguser2").classList.add('error');
+            errores.push('Debe introducir un correo electronico valido y repetirlo correctamente');
+            displayerr();
             return false;
         }
     }
-    if (password != password2 || password.length < 6) {
+    if (password != password2 || password.length < 8) {
         document.getElementById("regpassword").classList.add('error');
         document.getElementById("regpassword2").classList.add('error');
+        errores.push('Las contraseñas deben coincidir y tener mas de 8 caracteres');
+        displayerr();
         return false;
     }
+
+
 
     //Si salio todo bien...
 
